c8794cca79d90346e940b7854a166d2ea57d9e8d
tolgaocal80 <tolgaocal80@gmail.com>
2022-02-02 19:46:30 +0300

    -

--- GercekHayattaSoyutlama/src/Main.java
+++ GercekHayattaSoyutlama/src/Main.java
@@ -1,8 +1,13 @@
 
 public class Main {
 	
+	/*
+	 * SOLID prensiblerini kullanmýs olduk burada open-close prensibini yani yazýlýmýmýz 
+	 * deðiþime acik hale geldi, sadece bir nesne turunu degistirerek istedigimiz user check 
+	 * sinifini kullanabiliyoruz.
+	 */
+	
 	public static void main(String[] args) {
-		
 		
 		SignUpManager manager = new SignUpManager(new ComplexUserCheckService());
 		
